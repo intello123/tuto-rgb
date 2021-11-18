dfd6dc302ac46545bbe35658c08ff8823ee5fa9b
intello123 <93282495+intello123@users.noreply.github.com>
2021-11-18 11:10:24 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
+let intensite = 0
 function bleu () {
     pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function blanc () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function mauve () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P8, 1)
 }
@@ -13,14 +24,19 @@ function vert () {
     pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P8, 0)
 }
-let intensite = 0
-basic.showNumber(intensite)
+basic.forever(function () {
+    basic.showNumber(intensite)
+})
 basic.forever(function () {
     intensite = pins.analogReadPin(AnalogPin.P0)
     if (intensite >= 700) {
         vert()
-    } else if (intensite < 700 && intensite > 300) {
+    } else if (intensite < 350 && intensite > 300) {
         bleu()
+    } else if (intensite < 450 && intensite > 350) {
+        mauve()
+    } else if (intensite < 700 && intensite > 450) {
+        blanc()
     } else {
         rouge()
     }
